bc3063252b64b10a08d12e25b33546d6cbdabd44
danielcobo <danielcobo@users.noreply.github.com>
2021-09-05 23:57:55 +0200

    Simplify code

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
-'use strict';
-
-var rmTrailing = function rmTrailing(str, substr, flag){
-	if (typeof str !== 'string'){
-		throw new Error('Invalid argument: str');
-	}
-	if (typeof substr !== 'string'){
-		throw new Error('Invalid argument: substr');
-	}
-	
-	var regex;
-	var pattern = '[' + substr + ']+$'
-	if (typeof flag === 'undefined'){
-		regex = RegExp(pattern);
-	}
-	else{
-		regex = RegExp(pattern + '$', flag);
-	} 
-	return str.replace(regex, '');
-}
+/**
+ * Remove trailing substring
+ * @param {string} str - input string
+ * @param {string} substr - substring to remove
+ * @param {string} flag - regex flag (for example i to ignore Case, see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/flags)
+ * @returns {string}
+ */
+const rmTrailing = function rmTrailing(str, substr, flag) {
+  const pattern = '[' + substr + ']+$';
+  const regex = RegExp(pattern + '$', flag);
+  return str.replace(regex, '');
+};
 
 module.exports = rmTrailing;
